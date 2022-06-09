74b4b27263401a43b9fb53b63c1de8975b9c1ac0
nyawiradev <njaginjoyce@gmail.com>
2022-06-10 00:57:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+// const drivers = ["A", "B", "C", "D"];
+const findMatching = (drivers, street) => {
+   const filterArr = drivers.filter((filtered) => {
+       return filtered.toUpperCase() === street.toUpperCase();
+       // return filtered.toUpperCase() === street.toUpperCase(); {
+       //     console.log('good');
+       // }
+   });
+   return filterArr;
+}
+
+const fuzzyMatch = (drivers, street) => {
+   const filterArr = drivers.filter((filtered) => {
+       return filtered[0] === street[0];
+   });
+   return filterArr;
+}
+
+const matchName = (drivers, street) => {
+   const filterArr = drivers.filter((filtered) => {
+       console.log(filtered.name);
+       return filtered.name === street;
+   });
+   return filterArr;
+}
